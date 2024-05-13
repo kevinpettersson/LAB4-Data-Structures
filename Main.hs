e1c3c7f3261f8708e1035592caf6967cb508c2c6
kevinpettersson <kevin.petterssons@hotmail.com>
2024-05-13 22:51:32 +0200

    fixat emty kanske?

---
+++ Main.hs
@@ -0,0 +1,20 @@
+module Main where
+    
+import Route
+import RouteGUI
+import Graph  -- Create a module and use a sensible graph representation
+
+shortestPath :: Graph a b -> a -> a -> Maybe ([a], b)
+shortestPath g from to = undefined -- TODO: implement Dijkstra's algorithm
+
+main :: IO ()
+main = undefined  -- TODO: read arguments, build graph, output shortest path
+
+startGUI :: IO ()
+startGUI = do
+  Right stops <- readStops "your-stops.txt"
+  Right lines <- readLines "your-lines.txt"
+  let graph = undefined
+  -- TODO: build your graph here using stops and lines
+  -- runGUI stops lines graph shortestPath
+
