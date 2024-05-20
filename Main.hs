c3ca4dc933b7c559b9c769cfdf987f19ef5417ed
kevinpettersson <kevin.petterssons@hotmail.com>
2024-05-21 00:31:06 +0200

    hatar haskell just nu inget fungerar god natt

--- Main.hs
+++ Main.hs
@@ -2,24 +2,23 @@
 module Main where
     
 import Route
-import RouteGUI
-import Graph -- Create a module and use a sensible graph representation
+--import RouteGUI
+import Graph qualified as G-- Create a module and use a sensible graph representation
 --import Data.PSQueue qualified as Q
 import AATree qualified as Q
 import Data.Map qualified as M
-import AATree (AATree)
 
-shortestPath :: Graph a b -> a -> a -> Maybe ([a], b)
+shortestPath :: G.Graph a b -> a -> a -> Maybe ([a], b)
 shortestPath g from to = undefined                                      
 
-dijkstra :: Graph a b -> a -> Map a (b, a)
+dijkstra :: G.Graph a b -> a -> M.Map a (b, a)
 dijkstra g from 
   | G.member g from = dijkstra' from g (M.singleton from (0, from)) (Q.singleton from)
   | otherwise       = error "finns ingen väg"
 
-dijkstra' :: a -> Graph a b -> Map a (b, a) -> AATree a -> Map a (b, a)
+dijkstra' :: a -> G.Graph a b -> M.Map a (b, a) -> Q.AATree a -> M.Map a (b, a)
 dijkstra' from g mapp pq
-  | Q.empty == pq    = mapp
+  | Q.isEmpty pq      = mapp
   | M.notMember from = undefined
   where 
     adjList = adj from g
@@ -27,13 +26,15 @@ dijkstra' from g mapp pq
 main :: IO ()
 main = undefined  -- TODO: read arguments, build graph, output shortest path
 
-
+{-
 startGUI :: IO ()
 startGUI = do
   Right stops <- readStops "your-stops.txt"
   Right lines <- readLines "your-lines.txt"
   let graph = undefined -- TODO: build your graph here using stops and lines
   runGUI stops lines graph shortestPath
+-}
+
 
 
 
